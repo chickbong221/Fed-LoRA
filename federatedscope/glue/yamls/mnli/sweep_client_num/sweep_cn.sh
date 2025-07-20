eed6cc0907e40cd5216045a64a9d7386fdc938fb
Tuan <letuanhf@gmail.com>
2025-07-20 10:18:38 +0900

    m

---
+++ federatedscope/glue/yamls/mnli/sweep_client_num/sweep_cn.sh
@@ -0,0 +1,4 @@
+python3 federatedscope/main.py --cfg federatedscope/glue/yamls/mnli/sweep_client_num/fedex-lora-10client.yaml
+python3 federatedscope/main.py --cfg federatedscope/glue/yamls/mnli/sweep_client_num/fedex-lora-50client.yaml
+python3 federatedscope/main.py --cfg federatedscope/glue/yamls/mnli/sweep_client_num/fedsa-lora-10client.yaml
+python3 federatedscope/main.py --cfg federatedscope/glue/yamls/mnli/sweep_client_num/fedsa-lora-50client.yaml
---
+++ federatedscope/glue/yamls/rte/sweep_lr/sweep_lr.sh
@@ -0,0 +1,4 @@
+python3 federatedscope/main.py --cfg federatedscope/glue/yamls/rte/sweep_lr/fedex-lora1e-2.yaml
+python3 federatedscope/main.py --cfg federatedscope/glue/yamls/rte/sweep_lr/fedex-lora1e-3.yaml
+python3 federatedscope/main.py --cfg federatedscope/glue/yamls/rte/sweep_lr/fedex-lora5e-4.yaml
+python3 federatedscope/main.py --cfg federatedscope/glue/yamls/rte/sweep_lr/fedex-lora5e-3.yaml
